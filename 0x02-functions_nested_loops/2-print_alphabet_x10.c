c54be1c922a377193eafa0d394b21d162d83331a
Luyolomhlomi <LuyoloKhanyisamhlomi@gmail.com>
2023-02-22 11:26:34 -0800

    alphabet 10x

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - function that print 10x alphabets
+ * Return: Alwys 0;
+*/
+
+void print_alphabet_x10(void)
+{
+	int a;
+	int b;
+
+	for (a = 1; a <= 10; a++)
+	{
+		for (b = 'a'; b <= 'z'; b++)
+			_putchar(b);
+		_putchar('\n');
+	}
+}
