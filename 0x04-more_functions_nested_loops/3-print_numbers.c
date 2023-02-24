2ab1a709449f1c93b50c65a7ff21c654e970f78e
Luyolomhlomi <LuyoloKhanyisamhlomi@gmail.com>
2023-02-24 04:33:29 -0800

    numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_number - print 0
+ * Return: void
+ *
+*/
+void print_number(void)
+{
+	char c;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar ('\n');
+}
