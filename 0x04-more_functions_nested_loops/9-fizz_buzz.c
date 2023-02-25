59c80c2e295f062e9d77565f1e729458ec6c3708
Luyolomhlomi <LuyoloKhanyisamhlomi@gmail.com>
2023-02-25 04:41:25 -0800

    fizzbuzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - print numbers from 1 to 100
+ * Return: Always 0
+*/
+int main(void)
+{
+	int i;
+	char a[] = "Fizz";
+	char b[] = "Buzz";
+	char ab[] = "FizzBuzz";
+
+	for (i = 1; 1 <= 100; i++)
+	{
+		if (i == 100)
+			printf("%s", b);
+		else if ((i % 3 == 0) && (i % 5 == 0))
+			printf("%s", ab);
+		else if (i % 3 == 0)
+			printf("%s", a);
+		else if (i % 5 == 0)
+			printf("%s", b);
+		else
+			printf("%d", i);
+	}
+	printf("\n");
+	return (0);
+}
